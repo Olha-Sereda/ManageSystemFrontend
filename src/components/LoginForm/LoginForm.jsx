f9c3cb35c94661eaefcb6524d2e0bf18eac2d256
Olha-Sereda <olchik.sereda@gmail.com>
2024-05-05 18:31:09 +0200

    good

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
@@ -6,20 +6,12 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useForm } from "react-hook-form"
-import { useLoginMutation } from "../../store/index";
+} from '@/components/ui/dialog';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useForm } from 'react-hook-form';
+import { useLoginMutation } from '../../store/index';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export default function LoginForm() {
@@ -28,54 +20,56 @@ export default function LoginForm() {
   const navigate = useNavigate();
 
   function onSubmit(data) {
-    login(data).unwrap().then((res) => {
-      console.log("Result:::::", res);
-      navigate("/servers");
-    }).catch((err) => {
-      console.log("Error", err);
-    });
+    login(data)
+      .unwrap()
+      .then((res) => {
+        console.log('Result:::::', res);
+        navigate('/servers');
+      })
+      .catch((err) => {
+        console.log('Error', err);
+      });
   }
 
-  console.log("Result", result);
-  console.log("LoginForm");
+  console.log('Result', result);
+  console.log('LoginForm');
 
-  const form = useForm({defaultValues: {email: "", password: ""}, mode: "onBlur"})
+  const form = useForm({ defaultValues: { email: '', password: '' }, mode: 'onBlur' });
   return (
-   
-        <Form {...form} className="">
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="email"
-              rules={{ required: "This field is required" }}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              rules={{ required: "This field is required" }}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </form>
-        </Form>
-  )
+    <Form {...form} className="">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          rules={{ required: 'This field is required' }}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          rules={{ required: 'This field is required' }}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </form>
+    </Form>
+  );
 }
