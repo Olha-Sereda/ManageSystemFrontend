7fc9fcec5f81da5a0aebefb5f937da9bd2c36b54
Olha-Sereda <olchik.sereda@gmail.com>
2024-05-05 18:48:20 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import {createBrowserRouter, Navigate, Outlet, RouterProvider} from "react-router-dom";
-import MainLayoutPage from "@/page/MainLayoutPage.jsx";
-import LoginPage from "@/page/pages/LoginPage.jsx";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom';
+import MainLayoutPage from '@/page/MainLayoutPage.jsx';
+import LoginPage from '@/page/pages/LoginPage.jsx';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function PrivateRoutes() {
-    const session = true;
-  
-    if(session) {
-        return <Outlet />
-    } else {
-        return <Navigate to={"/login"} />
-    }
+  const session = true;
+
+  if (session) {
+    return <Outlet />;
+  } else {
+    return <Navigate to={'/login'} />;
+  }
 }
 
 function App() {
-
   return (
-
     <Router>
       <Routes>
-        <Route path={"/"} element={<PrivateRoutes />} >
-            <Route path={"/*"} element={<MainLayoutPage />} />
+        <Route path={'/'} element={<PrivateRoutes />}>
+          <Route path={'/*'} element={<MainLayoutPage />} />
         </Route>
-        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={'/login'} element={<LoginPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
