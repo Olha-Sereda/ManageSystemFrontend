b22c2762db4099c9a21da519a74ffbe856ea51af
Olha Sereda <olchik.sereda@gmail.com>
2024-05-08 00:03:56 +0200

    fix

---
+++ src/components/AddTemplateForm/AddTemplateForm.jsx
@@ -0,0 +1,64 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { useAddServiceMutation } from '../../store/apis/servicesApi';
+import { useParams } from 'react-router-dom';
+
+function AddTemplateForm({ children }) {
+  const [addService, result] = useAddServiceMutation();
+  const { id } = useParams();
+
+  function onSubmit(data) {
+    addService(data);
+  }
+
+  console.log('Result', result);
+
+  const form = useForm({ defaultValues: { service_name: '', serverId: id }, mode: 'onBlur' });
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <DialogHeader>
+              <DialogTitle>Create a test</DialogTitle>
+              <DialogDescription>
+                Create a new test for your service here. Click save when you&apos;re done.
+              </DialogDescription>
+            </DialogHeader>
+            <FormField
+              control={form.control}
+              name="service_name"
+              rules={{ required: 'This field is required' }}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Service name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export { AddTemplateForm };
